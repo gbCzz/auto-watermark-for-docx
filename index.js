4681c455b88273b83f85e60e9d74974edd86198f
Czz <Eason0208@163.com>
2026-03-12 23:54:56 +0800

    添加批量处理函数

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
 import * as fs from 'fs';
 
 import { FabricText, StaticCanvas } from 'fabric/node';
+import { PDFDocument } from 'pdf-lib';
 
 import { parseIni } from './utils.js';
-import { object } from 'webidl-conversions';
 
 class AutoBatchWatermark {
-  docx_buffer = null;
+  pdf_path = '';
+  pdf_buffer = null;
   wm_txt_list = null;
   /**
    * @type {import('fabric').TextProps}
    */
   wm_font_opt = {};
   /**
-   * 构造函数，读取 docx 文档，读取水印文件转换为列表
-   * @param {string} docx_path 需要添加水印的文档的路径
+   * 构造函数，读取 pdf 文档，读取水印文件转换为列表
+   * @param {string} pdf_path 需要添加水印的文档的路径
    * @param {string} wm_txt_path 水印列表 txt 的路径
    * @param {*} font_opt 水印的文字样式
    * @returns
    */
-  constructor(docx_path, wm_txt_path, font_opt) {
+  constructor(pdf_path, wm_txt_path, font_opt) {
+    this.pdf_path = pdf_path;
     Object.assign(this.wm_font_opt, font_opt);
-    console.log(`> 正在读取源文档：${docx_path}`);
+    console.log(`> 正在读取源文档：${pdf_path}`);
     try {
-      fs.accessSync(docx_path);
+      fs.accessSync(pdf_path);
     } catch (err) {
-      console.log(`> 未找到源文档：${docx_path}`);
+      console.log(`> 未找到源文档：${pdf_path}`);
       console.log('> 初始化失败');
       return;
     }
 
     try {
-      this.docx_buffer = fs.readFileSync(docx_path);
+      this.pdf_buffer = fs.readFileSync(pdf_path);
     } catch (err) {
       console.log(`> 读取时遇到错误：${err}`);
       console.log('> 初始化失败');
@@ -79,14 +81,46 @@ class AutoBatchWatermark {
     return Buffer.from(data_url.replace(/^data:image\/png;base64,/, ''), 'base64');
   }
 
-  
+  async addWmToPDF() {
+    if (this.wm_txt_list?.length == 0) return;
+
+    console.log(`> 正在加载源 PDF`);
+    const src_pdf_doc = await PDFDocument.load(this.pdf_buffer);
+
+    for (const txt_str of this.wm_txt_list) {
+      console.log(`> 正在生成水印图片：${txt_str}`);
+      const wm_buffer = await this.generateFabricWm(txt_str);
+
+      console.log(`> 正在将水印添加至文档`);
+      const pdf_doc = await src_pdf_doc.copy();
+      const png_img = await pdf_doc.embedPng(wm_buffer);
+      const { width: img_w, height: img_h } = png_img.scale(1.0);
+
+      const page_list = pdf_doc.getPages();
+
+      for (const page of page_list) {
+        const { width: page_w, height: page_h } = page.getSize();
+
+        page.drawImage(png_img, {
+          x: (page_w - img_w) / 2,
+          y: (page_h - img_h) / 2,
+          width: img_w,
+          height: img_h,
+          opacity: 1.0,
+        });
+      }
+
+      const pdf_buf = await pdf_doc.save();
+      fs.writeFileSync(`./${this.pdf_path.slice(0, this.pdf_path.length - 4)}-${txt_str}.pdf`, pdf_buf);
+      console.log(`> 成功生成带水印的 PDF: ./${this.pdf_path.slice(0, this.pdf_path.length - 4)}-${txt_str}.pdf`);
+    }
+  }
 }
 
 const run = async () => {
-  let abw = new AutoBatchWatermark('./教育学.docx', './一期班名单.txt', {
-    fontFamily: 'Microsoft YaHei',
-    fontSize: 72,
-    fontWeight: 'bold',
+  let abw = new AutoBatchWatermark('./教育学.pdf', './一期班名单.txt', {
+    fontFamily: '"宋体"',
+    fontSize: 108,
     fill: 'rgba(180, 180, 180, 0.4)',
     angle: -30,
     originX: 'center',
@@ -95,14 +129,8 @@ const run = async () => {
     top: 1000 / 2,
   });
 
-  if (abw.wm_txt_list && abw.wm_txt_list.length > 0) {
-    console.log(`\n> 开始为 "${abw.wm_txt_list[0]}" 生成水印图片...`);
-    const imgBuffer = await abw.generateFabricWm(abw.wm_txt_list[0]);
-
-    console.log(`> 水印图片生成成功！Buffer 大小: ${imgBuffer.length} bytes`);
-
-    // 你可以在这里加一行 fs.writeFileSync('test.png', imgBuffer) 来在本地预览一下图片效果！
-  }
+  abw.addWmToPDF();
 };
 
 run();
+

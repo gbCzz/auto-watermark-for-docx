ae2002562b3f3658c0f5c1cc7812f87202f287ca
Czz <Eason0208@163.com>
2026-03-12 16:47:49 +0800

    完成 AutoBatchWatermark 类的构造函数

--- index.js
+++ index.js
@@ -1,27 +1,56 @@
 import * as fs from 'fs';
 
-class WatermarkCfg {
-	docx_buffer = null;
-	wm_txt_list = null;
+import { parseIni } from './utils.js';
 
-	constructor(docx_path, wm_txt_path) {
-		console.log(`正在读取源文档：${docx_path}`);
-		if (!fs.accessSync(docx_path)) {
-			console.log(`未找到源文档：${docx_path}`);
-		}
+class AutoBatchWatermark {
+  docx_buffer = null;
+  wm_txt_list = null;
 
-		try {
-			this.docx_buffer = fs.readFileSync(docx_path);
-		} catch (err) {
-			console.log(`读取时遇到错误：${err}`);
-			console.log('初始化失败');
-			return;
-		}
+  /**
+   * 构造函数，读取 docx 文档，读取水印文件转换为列表
+   * @param {string} docx_path 需要添加水印的文档的路径
+   * @param {string} wm_txt_path 水印列表 txt 的路径
+   * @returns 
+   */
+  constructor(docx_path, wm_txt_path) {
+    console.log(`> 正在读取源文档：${docx_path}`);
+    try {
+      fs.accessSync(docx_path);
+    } catch (err) {
+      console.log(`> 未找到源文档：${docx_path}`);
+      console.log('> 初始化失败');
+      return;
+    }
 
-		try {
-			let rawtxt = fs.readFileSync(wm_txt_path).toString();
-			let reg = RegExp('(\w+)\s*=\s*(\S+)');
-		} catch (err) {}
-	}
+    try {
+      this.docx_buffer = fs.readFileSync(docx_path);
+    } catch (err) {
+      console.log(`> 读取时遇到错误：${err}`);
+      console.log('> 初始化失败');
+      return;
+    }
+    console.log('> 成功读取源文档');
+
+    console.log(`> 正在读取水印列表：${wm_txt_path}`);
+    try {
+      fs.accessSync(wm_txt_path);
+    } catch (err) {
+      console.log(`> 未找到水印列表：${wm_txt_path}`);
+      console.log('> 初始化失败');
+	  return;
+    }
+
+    try {
+      let rawtxt = fs.readFileSync(wm_txt_path).toString();
+      this.wm_txt_list = rawtxt.split(/[\s*?\r?\n]+/);
+    } catch (err) {
+      console.log(`> 读取时遇到错误：${err}`);
+      console.log('> 初始化失败');
+      return;
+    }
+    console.log('> 成功读取水印列表，需要添加的水印如下：');
+    for (const wm_txt of this.wm_txt_list) console.log(wm_txt);
+  }
 }
 
+let abw = new AutoBatchWatermark('./教育学.docx', './一期班名单.txt');
